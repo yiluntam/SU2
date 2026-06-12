cdb9e2d1cb56f1d4fdc7e50827ffa64a3d008439
yiluntam <tylunlunlun@163.com>
2026-06-12 21:17:37 +0800

    Do not filter surface CSV fields by compact restart setting
    WRT_RESTART_COMPACT only affects restart files,
    surface CSV should always output complete fields.

--- SU2_CFD/src/output/COutput.cpp
+++ SU2_CFD/src/output/COutput.cpp
@@ -405,10 +405,6 @@ void COutput::WriteToFile(CConfig *config, CGeometry *geometry, OUTPUT_TYPE form
       if (!config->GetWrt_Surface_Overwrite())
         filename_iter = config->GetFilename_Iter(fileName, curInnerIter, curOuterIter);
 
-      /*--- If we have compact restarts, we use only the required fields. ---*/
-      if (config->GetWrt_Restart_Compact())
-        surfaceDataSorter->SetRequiredFieldNames(requiredVolumeFieldNames);
-
       surfaceDataSorter->SortConnectivity(config, geometry);
       surfaceDataSorter->SortOutputData();
 
